733f7a8cc6ca4766f4d17bf80df89f5f2991b12f
olsen3d <olsen3d@gmail.com>
2021-02-21 22:37:02 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ const statsFPS = new Stats()
 statsFPS.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild( statsFPS.dom )
 
+const debugArea = document.querySelector('#debug')
+
 if (WEBGL.isWebGLAvailable()) {
   let camera, scene, renderer
   let modelReady = false
@@ -91,61 +93,56 @@ if (WEBGL.isWebGLAvailable()) {
   }
 
   function onWindowResize() {
-    // camera.aspect = window.innerWidth / window.innerHeight
     camera.aspect = 720 / 480
     camera.updateProjectionMatrix()
-
     renderer.setSize(window.innerWidth, window.innerHeight)
   }
 
-
-
-
-const mouse = new THREE.Vector2()
-function onDocumentMouseMove(event) {
-  event.preventDefault();
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-}
-document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-let hoverHeight = {
-  normal: 100,
-  normalMax: 300,
-  hoverAmount: 0,
-  mouseAmount: 0,
-  hoverMin: 10,
-  hoverMax: 25,
-  mouseMax: 150,
-  currentX: function() {
-    return this.normal + this.hoverAmount + this.mouseAmount
+  const mouse = new THREE.Vector2()
+  function onDocumentMouseMove(event) {
+    event.preventDefault();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   }
-}
-
-const maxHorizontalPosition = 1000
-const updateHoverMousePosition = () => {
-  gsap.to(ingenuityController.position, { duration: 5, ease: 'power2.out', x: mouse.x * maxHorizontalPosition })
-  gsap.to(hoverHeight, { duration: 5, ease: 'power2.out', mouseAmount: mouse.y * hoverHeight.mouseMax })
-}
-
-const updateHoverMouseRotation = () => {
-  const distance = (mouse.x * maxHorizontalPosition) - ingenuityController.position.x
-  ingenuityController.rotation.z = THREE.Math.degToRad(distance / -40)
-}
-
-const takeOff = () => {
-  gsap.to(hoverHeight, { duration: 2, ease: 'power1.inOut', normal: hoverHeight.normalMax })
-  gsap.to(ingenuityController.rotation, { duration: 4, ease: 'back.inOut(4)', y: 0 })
-}
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
 
 
-const hover = () => {
-  let isUp = false
-  let amount = (Math.random() * hoverHeight.hoverMax) + hoverHeight.hoverMin
-
-  const updateHoverVerticalPosition = (amount) => {
-    gsap.to(hoverHeight, { duration: 4, ease: 'back.inOut(4)', hoverAmount: amount })
+  let hoverHeight = {
+    normal: 100,
+    normalMax: 300,
+    hoverAmount: 0,
+    mouseAmount: 0,
+    hoverMin: 10,
+    hoverMax: 25,
+    mouseMax: 150,
+    currentX: function() {
+      return this.normal + this.hoverAmount + this.mouseAmount
+    }
   }
+
+  const maxHorizontalPosition = 1000
+  const updateHoverMousePosition = () => {
+    gsap.to(ingenuityController.position, { duration: 5, ease: 'power2.out', x: mouse.x * maxHorizontalPosition })
+    gsap.to(hoverHeight, { duration: 5, ease: 'power2.out', mouseAmount: mouse.y * hoverHeight.mouseMax })
+  }
+
+  const updateHoverMouseRotation = () => {
+    const distance = (mouse.x * maxHorizontalPosition) - ingenuityController.position.x
+    ingenuityController.rotation.z = THREE.Math.degToRad(distance / -40)
+  }
+
+  const takeOff = () => {
+    gsap.to(hoverHeight, { duration: 2, ease: 'power1.inOut', normal: hoverHeight.normalMax })
+    gsap.to(ingenuityController.rotation, { duration: 4, ease: 'back.inOut(4)', y: 0 })
+  }
+
+  const hover = () => {
+    let isUp = false
+    let amount = (Math.random() * hoverHeight.hoverMax) + hoverHeight.hoverMin
+
+    const updateHoverVerticalPosition = (amount) => {
+      gsap.to(hoverHeight, { duration: 4, ease: 'back.inOut(4)', hoverAmount: amount })
+    }
 
   window.setInterval(() => {
     isUp = !isUp
@@ -158,12 +155,18 @@ const hover = () => {
   }, 4000)
 }
 
+  const updateCamera = () => {
+    const maxRotation = 200
+    gsap.to(camera.rotation, { duration: 7, ease: 'power1.out', y: mouse.x * maxRotation * 0.001 * -1 })
+  }
 
-const updateCamera = () => {
-  const maxRotation = 200
-  gsap.to(camera.rotation, { duration: 7, ease: 'power1.out', y: mouse.x * maxRotation * 0.001 * -1 })
-}
-
+  const updateRotors = () => {
+    if (rotor1.rotation.x > 360) rotor1.rotation.x = 0
+    if (rotor2.rotation.x > 360) rotor2.rotation.x = 0
+    rotor1.rotation.x += 0.3
+    rotor2.rotation.x -= 0.4
+    debugArea.innerHTML = rotor1.rotation.x
+  }
 
 
 
@@ -175,30 +178,30 @@ const updateCamera = () => {
 
 
   let startTakeOff = false
+  let inFlight = false
 
   const update = () => {
     if (modelReady && !startTakeOff) {
-      setTimeout(() => {
-        startTakeOff = true
-      }, 1000)
+      setTimeout(() => {startTakeOff = true}, 1000)
+      setTimeout(() => {inFlight = true}, 2000)
       takeOff()
       hover()
     }
 
+    ingenuityController.position.y = hoverHeight.currentX()
     if (modelReady) {
+      updateRotors()
+    }
+    if (modelReady && inFlight) {
       updateCamera()
-      ingenuityController.position.y = hoverHeight.currentX()
       updateHoverMouseRotation()
       updateHoverMousePosition()
-      rotor1.rotation.x += 0.3
-      rotor2.rotation.x -= 0.4
     }
   }
 
-  const fixedUpdate = () => {}
-
-  const fixedUpdateFrequency = 33.3 //33.3ms ~30FPS
-  window.setInterval(fixedUpdate, fixedUpdateFrequency)
+  //const fixedUpdate = () => {}
+  //const fixedUpdateFrequency = 33.3 //33.3ms ~30FPS
+  //window.setInterval(fixedUpdate, fixedUpdateFrequency)
 
   const render = () => renderer.render(scene, camera)
 
